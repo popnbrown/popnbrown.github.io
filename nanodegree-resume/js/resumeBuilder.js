9b240c4cb8d7a70552066fec23877ee18f096ee1
Sravan Suryadevara <sravan.suryadevara@gmail.com>
2015-10-13 09:04:47 -0500

    changes

--- nanodegree-resume/js/resumeBuilder.js
+++ nanodegree-resume/js/resumeBuilder.js
@@ -1,145 +1,149 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 'use strict';
+
  var bio = {
- 	"name": "Sravan K. Suryadevara",
- 	"role": "Front-End Ninjaneer",
- 	"contacts": {
- 		"mobile": "908-432-5330",
- 		"email": "sravan.suryadevara@gmail.com",
- 		"github": "https://github.com/popnbrown",
- 		"twitter": "https://twitter.com/popnbrown",
- 		"location": "Chicago, IL"
+ 	'name': 'Sravan K. Suryadevara',
+ 	'role': 'Front-End Ninjaneer',
+ 	'contacts': {
+ 		'mobile': '908-432-5330',
+ 		'email': 'sravan.suryadevara@gmail.com',
+ 		'github': 'https://github.com/popnbrown',
+ 		'twitter': 'https://twitter.com/popnbrown',
+ 		'location': 'Chicago, IL'
  	},
- 	"welcomeMessage": "Welcome to my Interactive Resume!",
- 	"skills": ["HTML", "CSS", "Javascript", "C#", "PHP"],
- 	"biopic": "https://media.licdn.com/mpr/mpr/shrinknp_300_300/p/3/005/0b9/23c/21e4704.jpg",
- 	display: function() {
-		$('#header').append(HTMLheaderName.replace("%data%", bio.name));
-		$('#header').append(HTMLheaderRole.replace("%data%", bio.role));
-		$('#topContacts').append(HTMLmobile.replace("%data%", bio.contacts.mobile));
-		$('#topContacts').append(HTMLemail.replace("%data%", bio.contacts.email));
-		$('#topContacts').append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
-		$('#topContacts').append(HTMLgithub.replace("%data%", bio.contacts.github));
-		$('#topContacts').append(HTMLlocation.replace("%data%", bio.contacts.location));
-		$('#header').append(HTMLbioPic.replace("%data%", bio.biopic));
-		$('#header').append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
-		$('#header').append(HTMLskillsStart);
+ 	'welcomeMessage': 'Welcome to my Interactive Resume!',
+ 	'skills': ['HTML', 'CSS', 'Javascript', 'C#', 'PHP'],
+ 	'biopic': 'https://media.licdn.com/mpr/mpr/shrinknp_300_300/p/3/005/0b9/23c/21e4704.jpg',
+ 	'display': function() {
+		$('#header').append(HTMLheaderName.replace('%data%', bio.name),
+							HTMLheaderRole.replace('%data%', bio.role),
+							HTMLbioPic.replace('%data%', bio.biopic),
+							HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage),
+							HTMLskillsStart);
+
 		$.each(bio.skills, function(index,value) {
-			$('#header').append(HTMLskills.replace("%data%",value));
+			$('#header').append(HTMLskills.replace('%data%',value));
 		});
+
+		$('#topContacts, #footerContacts').append(HTMLmobile.replace('%data%', bio.contacts.mobile),
+												  HTMLemail.replace('%data%', bio.contacts.email),
+												  HTMLtwitter.replace('%data%', bio.contacts.twitter),
+												  HTMLgithub.replace('%data%', bio.contacts.github),
+												  HTMLlocation.replace('%data%', bio.contacts.location));
  	}
  };
 
  var education = {
- 	"schools": [
+ 	'schools': [
  		{
- 			"name": "University of Illinois at Urbana-Champaign",
- 			"location": "Urbana, IL",
- 			"degree": "Bachelor of Science",
- 			"majors": ["Computer Science", "Ramen Noodles"],
- 			"dates": 2013,
- 			"url": "https://Illinois.edu"
+ 			'name': 'University of Illinois at Urbana-Champaign',
+ 			'location': 'Urbana, IL',
+ 			'degree': 'Bachelor of Science',
+ 			'majors': ['Computer Science', 'Ramen Noodles'],
+ 			'dates': 2013,
+ 			'url': 'https://Illinois.edu'
  		},
  		{
- 			"name": "Montgomery High School",
- 			"location": "Skillman, NJ",
- 			"degree": "General Education",
- 			"majors": ["General"],
- 			"dates": 2009,
- 			"url": "http://mtsd.k12.nj.us"
+ 			'name': 'Montgomery High School',
+ 			'location': 'Skillman, NJ',
+ 			'degree': 'General Education',
+ 			'majors': ['General'],
+ 			'dates': 2009,
+ 			'url': 'http://mtsd.k12.nj.us'
  		}
  	],
- 	"onlineCourses": [
+ 	'onlineCourses': [
  		{
- 			"title": "Responsive Images",
- 			"school": "Udacity",
- 			"date": 2015,
- 			"schoolurl": "https://udacity.com",
- 			"url": "https://www.udacity.com/course/responsive-images--ud882"
+ 			'title': 'Responsive Images',
+ 			'school': 'Udacity',
+ 			'date': 2015,
+ 			'schoolurl': 'https://udacity.com',
+ 			'url': 'https://www.udacity.com/course/responsive-images--ud882'
  		},
  		{
- 			"title": "Introduction to Key Constitutional Concepts and Supreme Court Cases",
- 			"school": "Coursera",
- 			"date": 2014,
- 			"schoolurl": "https://coursera.com",
- 			"url": "https://www.coursera.org/course/constitution"
+ 			'title': 'Introduction to Key Constitutional Concepts and Supreme Court Cases',
+ 			'school': 'Coursera',
+ 			'date': 2014,
+ 			'schoolurl': 'https://coursera.com',
+ 			'url': 'https://www.coursera.org/course/constitution'
  		}
  	],
- 	display: function() {
+ 	'display': function() {
  		$.each(education.schools, function(index, value) {
 			$('#education').append(HTMLschoolStart);
-			$('.education-entry:last').append(HTMLschoolName.replace("%data%",value.name).replace("#",value.url));
-			$('.education-entry:last').append(HTMLschoolDegree.replace("%data%",value.degree));
-			$('.education-entry:last').append(HTMLschoolDates.replace("%data%",value.dates));
-			$('.education-entry:last').append(HTMLschoolLocation.replace("%data%",value.location));
+			$('.education-entry:last').append(HTMLschoolName.replace('%data%',value.name).replace('#',value.url),
+											  HTMLschoolDegree.replace('%data%',value.degree),
+											  HTMLschoolDates.replace('%data%',value.dates),
+											  HTMLschoolLocation.replace('%data%',value.location));
 			$.each(value.majors, function(index, value) {
-				$('.education-entry:last').append(HTMLschoolMajor.replace("%data%",value));
+				$('.education-entry:last').append(HTMLschoolMajor.replace('%data%',value));
 			});
  		});
  		$('#education').append(HTMLonlineClasses);
  		$.each(education.onlineCourses, function(index, value) {
 			$('#education').append(HTMLschoolStart);
-			$('.education-entry:last').append(HTMLonlineTitle.replace("%data%",value.title).replace("#",value.schoolurl));
-			$('.education-entry:last').append(HTMLonlineSchool.replace("%data%",value.school));
-			$('.education-entry:last').append(HTMLonlineDates.replace("%data%",value.date));
-			$('.education-entry:last').append(HTMLonlineURL.replace("%data%",value.url).replace("#",value.url));
- 		})
+			$('.education-entry:last').append(HTMLonlineTitle.replace('%data%',value.title).replace('#',value.schoolurl),
+											  HTMLonlineSchool.replace('%data%',value.school),
+											  HTMLonlineDates.replace('%data%',value.date),
+											  HTMLonlineURL.replace('%data%',value.url).replace('#',value.url));
+ 		});
  	}
  };
 
  var work = {
- 	"jobs": [
+ 	'jobs': [
  		{
- 			"employer": "One North Interactive",
- 			"title": "Developer",
- 			"location": "Chicago, IL",
- 			"dates": "July 1, 2013 - August 15, 2015",
- 			"description": "Built medium to large web application and assisted in ongoing maintenance"
+ 			'employer': 'One North Interactive',
+ 			'title': 'Developer',
+ 			'location': 'Chicago, IL',
+ 			'dates': 'July 1, 2013 - August 15, 2015',
+ 			'description': 'Built medium to large web application and assisted in ongoing maintenance'
  		},
  		{
- 			"employer": "FIRST",
- 			"title": "Drupal Developer",
- 			"location": "Manchester, NH",
- 			"dates": "August 15, 2015 - ",
- 			"description": "Assist in redesigning new site, and maintaining all website assets"
+ 			'employer': 'FIRST',
+ 			'title': 'Drupal Developer',
+ 			'location': 'Manchester, NH',
+ 			'dates': 'August 15, 2015 - ',
+ 			'description': 'Assist in redesigning new site, and maintaining all website assets'
  		}
  	],
  	display: function(){
  		$.each(work.jobs, function(index, value) {
 			$('#workExperience').append(HTMLworkStart);
-			$('.work-entry:last').append(HTMLworkEmployer.replace("%data%",value.employer));
-			$('.work-entry:last').append(HTMLworkTitle.replace("%data%",value.title));
-			$('.work-entry:last').append(HTMLworkDates.replace("%data%",value.dates));
-			$('.work-entry:last').append(HTMLworkLocation.replace("%data%",value.location));
-			$('.work-entry:last').append(HTMLworkDescription.replace("%data%",value.description));
+			$('.work-entry:last').append(HTMLworkEmployer.replace('%data%',value.employer),
+										 HTMLworkTitle.replace('%data%',value.title),
+										 HTMLworkDates.replace('%data%',value.dates),
+										 HTMLworkLocation.replace('%data%',value.location),
+										 HTMLworkDescription.replace('%data%',value.description));
  		});
  	}
  };
 
  var projects = {
- 	"projects": [
+ 	'projects': [
  		{
- 			"title": "Illinois FIRST Maps",
- 			"dates": "December 2013 - March 2014",
- 			"description": "Created a listing of all Illinois FIRST teams in a map",
- 			"images": ["http://popnbrown.github.io/nanodegree-resume/images/ilfirstmaps1-300.png","http://popnbrown.github.io/nanodegree-resume/images/ilfirstmaps2-300.png"]
+ 			'title': 'Illinois FIRST Maps',
+ 			'dates': 'December 2013 - March 2014',
+ 			'description': 'Created a listing of all Illinois FIRST teams in a map',
+ 			'images': ['http://popnbrown.github.io/nanodegree-resume/images/ilfirstmaps1-300.png','http://popnbrown.github.io/nanodegree-resume/images/ilfirstmaps2-300.png']
  		},
  		{
- 			"title": "The Blue Alliance",
- 			"dates": "December 2013 - Present",
- 			"description": "Help with bugs for the TBA web app",
- 			"images": ["http://popnbrown.github.io/nanodegree-resume/images/tba1-300.png"]
+ 			'title': 'The Blue Alliance',
+ 			'dates': 'December 2013 - Present',
+ 			'description': 'Help with bugs for the TBA web app',
+ 			'images': ['http://popnbrown.github.io/nanodegree-resume/images/tba1-300.png']
  		}
  	],
  	display: function(){
  		$.each(projects.projects, function(index, value) {
 			$('#projects').append(HTMLprojectStart);
-			$('.project-entry:last').append(HTMLprojectTitle.replace("%data%",value.title));
-			$('.project-entry:last').append(HTMLprojectDates.replace("%data%",value.dates));
-			$('.project-entry:last').append(HTMLprojectDescription.replace("%data%",value.description));
+			$('.project-entry:last').append(HTMLprojectTitle.replace('%data%',value.title),
+											HTMLprojectDates.replace('%data%',value.dates),
+											HTMLprojectDescription.replace('%data%',value.description));
 			$.each(value.images, function(index, value) {
-				$('.project-entry:last').append(HTMLprojectImage.replace("%data%",value));
+				$('.project-entry:last').append(HTMLprojectImage.replace('%data%',value));
 			});
  		});
  	}
@@ -151,4 +155,4 @@ education.display();
 work.display();
 projects.display();
 
-$("#mapDiv").append(googleMap);
+$('#mapDiv').append(googleMap);
