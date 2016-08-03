1e2bceb2f1ab8031aed8a68a224f9f16b271e6b8
Sravan Suryadevara <sravan.suryadevara@gmail.com>
2016-08-03 17:55:39 -0500

    make only 32 pizzas

--- mobile-portfolio/views/js/main.js
+++ mobile-portfolio/views/js/main.js
@@ -525,7 +525,7 @@ window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-  var cols = 8;
+  var cols = 4;
   var s = 256;
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
