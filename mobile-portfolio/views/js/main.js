3427534e52f81efa42b853b7d8dee708df7742d2
Sravan Suryadevara <sravan.suryadevara@gmail.com>
2016-08-03 18:25:13 -0500

    add style left

--- mobile-portfolio/views/js/main.js
+++ mobile-portfolio/views/js/main.js
@@ -534,6 +534,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
+    elem.style.left = (i % cols) * s;
     elem.style.transform = "translate(" + elem.basicLeft + ")";
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
