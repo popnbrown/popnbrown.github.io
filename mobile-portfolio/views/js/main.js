d7c7b71c1eede80d71788cf3736390d9d2000fe1
Sravan Suryadevara <sravan.suryadevara@gmail.com>
2016-08-03 18:30:12 -0500

    add px for left

--- mobile-portfolio/views/js/main.js
+++ mobile-portfolio/views/js/main.js
@@ -533,7 +533,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
-    elem.style.left = (i % cols) * s;
+    elem.style.left = (i % cols) * s + 'px';
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
